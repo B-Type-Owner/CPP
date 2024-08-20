0be9437a7d517f1092d9b6a99cc29c5bfe209271
JeonDY <jeondayoon27@gmail.com>
2024-08-20 11:13:40 +0900

    삼국지게임

--- DAYUN/삼국지게임/solution.cpp
+++ DAYUN/삼국지게임/solution.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <unordered_map>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -9,18 +10,16 @@ int soldier[25][25];
 int group_id[25][25];
 vector<pair<int, int>> group[625];
 vector<int> enemy[625];
-int group_recruit[625];
-int sizeN;
+int sizeN = 0;
 
 void init(int N, int mSoldier[25][25], char mMonarch[25][25][11])
 {
 	sizeN = N;
 	monarches_area.clear();
-	memset(group_recruit, 0, sizeof(group_recruit));
 	int group_cnt = 0;
 	for (int i = 0; i < N; ++i) {
 		for (int j = 0; j < N; ++j) {
-			monarches_area.insert({ string(mMonarch[i][j]), {i, j}});
+			monarches_area.insert({ string(mMonarch[i][j]), {i, j} });
 			soldier[i][j] = mSoldier[i][j];
 			group_id[i][j] = group_cnt++;
 			group[group_id[i][j]].push_back({ i, j });
@@ -30,7 +29,10 @@ void init(int N, int mSoldier[25][25], char mMonarch[25][25][11])
 
 void destroy()
 {
-
+	for (int i = 0; i < (sizeN * sizeN); ++i) {
+		group[i].clear();
+		enemy[i].clear();
+	}
 }
 
 int ally(char mMonarchA[11], char mMonarchB[11])
@@ -39,13 +41,13 @@ int ally(char mMonarchA[11], char mMonarchB[11])
 	pair<int, int> a = monarches_area[string(mMonarchA)];
 	pair<int, int> b = monarches_area[string(mMonarchB)];
 
-	//ÀÌ¹Ì °°Àº ±×·ìÀÎÁö È®ÀÎ
+	//ì´ë¯¸ ê°™ì€ ê·¸ë£¹ì¸ì§€ í™•ì¸
 	int a_group = group_id[a.first][a.second];
 	int b_group = group_id[b.first][b.second];
-	if (a_group==b_group) {
+	if (a_group == b_group) {
 		return -1;
 	}
-	//Àû ±×·ìÀÎÁö È®ÀÎ
+	//ì  ê·¸ë£¹ì¸ì§€ í™•ì¸
 	for (int i = 0; i < enemy[a_group].size(); ++i) {
 		if (enemy[a_group][i] == b_group)
 			return -2;
@@ -54,12 +56,14 @@ int ally(char mMonarchA[11], char mMonarchB[11])
 		group[a_group].push_back(group[b_group][i]);
 		group_id[group[b_group][i].first][group[b_group][i].second] = a_group;
 	}
+
 	for (int i = 0; i < enemy[b_group].size(); ++i) {
 		enemy[a_group].push_back(enemy[b_group][i]);
 		enemy[enemy[b_group][i]].push_back(a_group);
 	}
 	return 1;
 }
+
 
 int attack(char mMonarchA[11], char mMonarchB[11], char mGeneral[11])
 {
@@ -68,21 +72,37 @@ int attack(char mMonarchA[11], char mMonarchB[11], char mGeneral[11])
 	pair<int, int> b = monarches_area[string(mMonarchB)];
 	int a_group = group_id[a.first][a.second];
 	int b_group = group_id[b.first][b.second];
-	
+
 	int attackSoliders = 0;
 	int defenseSoliders = 0;
 	if (a_group == b_group) {
 		return -1;
 	}
 
-	//Àû´ë°ü°è Á¤ÀÇ
+	//ì¡´ì¬í•˜ëŠ”ì§€ í™•ì¸
+	bool toggle = false;
+	for (int i = b.first - 1; i <= (b.first + 1); ++i) {
+		if (i < 0 || i >= sizeN) continue;
+		for (int j = b.second - 1; j <= (b.second + 1); ++j) {
+			if (j < 0 || j >= sizeN) continue;
+			int gi = group_id[i][j];
+			if (a_group == gi) {
+				toggle = true;
+				i += 3;
+				break;
+			}
+		}
+	}
+	if (!toggle) return -2;
+
+	//ì ëŒ€ê´€ê³„ ì •ì˜
 	enemy[a_group].push_back(b_group);
 	enemy[b_group].push_back(a_group);
 
 
 	for (int i = b.first - 1; i <= (b.first + 1); ++i) {
 		if (i < 0 || i >= sizeN) continue;
-		for (int j = b.second -1; j <= (b.second + 1); ++j) {
+		for (int j = b.second - 1; j <= (b.second + 1); ++j) {
 			if (j < 0 || j >= sizeN) continue;
 			int gi = group_id[i][j];
 			if (a_group == gi) {
@@ -104,6 +124,13 @@ int attack(char mMonarchA[11], char mMonarchB[11], char mGeneral[11])
 	else {
 		soldier[b.first][b.second] = attackSoliders - defenseSoliders;
 		monarches_area.insert({ string(mGeneral), {b.first, b.second} });
+		group_id[b.first][b.second] = a_group;
+		group[a_group].push_back({ b.first, b.second });
+		for (int i = 0; i < group[b_group].size(); ++i) {
+			if (b == group[b_group][i]) {
+				group[b_group].erase(group[b_group].begin() + i);
+			}
+		}
 		return 1;
 	}
 
